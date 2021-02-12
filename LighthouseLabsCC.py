80fb7f7e5d7b551c07995507eb6e11219088747b
Mark <ly.mark@gmail.com>
2021-02-12 15:18:09 -0800

    Lighthouse labs Python crash course exercise 1 string replacement

---
+++ LighthouseLabsCC.py
@@ -0,0 +1,12 @@
+journey = """Just a small tone girl
+Leaving in a lonely whirl
+She took the midnight tray going anywhere
+Just a seedy boy
+Bored and raised in South Detroit or something
+He took the midnight tray going anywhere"""
+
+journey = journey.replace("tone",'time')
+journey = journey.replace('whirl','world')
+journey = journey.replace('tray','train', 2)
+journey = journey.replace('seedy','city')
+journey = journey.replace('or something',"")
